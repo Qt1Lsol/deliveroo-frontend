9040d763265263f45450aca22ef775f2f95ca2d3
unknown <qlagache@gmail.com>
2022-02-08 11:34:56 +0100

    first save

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Hero from "./components/Hero";
+import Category from "./components/Category";
 
 function App() {
-  return (
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        "https://lereacteur-deliveroo-api.herokuapp.com"
+      );
+      // console.log(response.data);
+      setData(response.data);
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  return isLoading ? (
+    <div>En cours de chargement...</div>
+  ) : (
     <div>
-      Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
+      <Hero restaurant={data.restaurant} />
+      {/* Liste de catégories */}
+      {data.categories.map((category, index) => {
+        return (
+          category.meals.length > 0 && (
+            <Category key={index} category={category} />
+          )
+        );
+      })}
     </div>
   );
 }
